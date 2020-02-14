50a9c66d0c8afb75f9862df7d77faced8b43147a
Sebastian Lauwers <sl@unticks.com>
2020-02-14 09:14:36 +0100

    Increase consistency of parsing methods

--- src/parser.rs
+++ src/parser.rs
@@ -6,9 +6,9 @@ use nom::{
     sequence::{ pair, tuple },
     branch::alt,
     multi::many0,
+    character::complete::one_of,
     bytes::complete::{
         tag,
-        is_a,
         take_while,
         take_while1,
         take_while_m_n,
@@ -17,39 +17,65 @@ use nom::{
 
 const RESERVED_CHARS: &'static [u8] = b";/?:@&=+$,";
 
-pub fn is_reserved(i: u8) -> bool {
-    RESERVED_CHARS.contains(&i)
-}
-
-pub fn reserved(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    is_a(RESERVED_CHARS)(input)
+pub fn reserved(input: &[u8]) -> IResult<&[u8], char> {
+    one_of(RESERVED_CHARS)(input)
 }
 
 const MARK_CHARS: &'static [u8] = b"-_.!~*'()";
 
-pub fn is_mark(i: u8) -> bool {
-    MARK_CHARS.contains(&i)
+pub fn mark(input: &[u8]) -> IResult<&[u8], char> {
+    one_of(MARK_CHARS)(input)
 }
 
-pub fn mark(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    is_a(MARK_CHARS)(input)
+const NUMERIC_CHARS: &'static [u8] = b"0123456789";
+
+pub fn numeric(input: &[u8]) -> IResult<&[u8], char> {
+    one_of(NUMERIC_CHARS)(input)
 }
 
-pub fn is_unreserved(i: u8) -> bool {
-    is_mark(i) || i.is_ascii_alphanumeric()
+const ALPHABETIC_CHARS: &'static [u8] = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+pub fn alphabetic(input: &[u8]) -> IResult<&[u8], char> {
+    one_of(ALPHABETIC_CHARS)(input)
 }
 
-pub fn unreserved(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    take_while(is_unreserved)(input)
+pub fn alphanumeric(input: &[u8]) -> IResult<&[u8], char> {
+    alt((numeric, alphabetic))(input)
 }
+
+const LOWERCASE_HEXADECIMAL_CHARS: &'static [u8] = b"abcdef";
+
+pub fn lowercase_hexadecimal(input: &[u8]) -> IResult<&[u8], char> {
+    alt((
+        numeric,
+        one_of(LOWERCASE_HEXADECIMAL_CHARS)
+    ))(input)
+}
+
+const UPPERCASE_HEXADECIMAL_CHARS: &'static [u8] = b"ABCDEF";
 
 pub fn is_hex_digit(i: u8) -> bool {
-    i.is_ascii_hexdigit()
+    NUMERIC_CHARS.contains(&i)
+        || LOWERCASE_HEXADECIMAL_CHARS.contains(&i)
+        || UPPERCASE_HEXADECIMAL_CHARS.contains(&i)
 }
 
-pub fn is_lowercase_hex_digit(i: u8) -> bool {
-    i.is_ascii_digit() ||
-        (i.is_ascii_hexdigit() && i.is_ascii_lowercase())
+pub fn uppercase_hexadecimal(input: &[u8]) -> IResult<&[u8], char> {
+    alt((
+        numeric,
+        one_of(UPPERCASE_HEXADECIMAL_CHARS)
+    ))(input)
+}
+
+pub fn hexadecimal(input: &[u8]) -> IResult<&[u8], char> {
+    alt((
+        lowercase_hexadecimal,
+        uppercase_hexadecimal,
+    ))(input)
+}
+
+pub fn unreserved(input: &[u8]) -> IResult<&[u8], char> {
+    alt((mark, alphanumeric))(input)
 }
 
 #[derive(PartialEq, Debug)]
@@ -82,29 +108,19 @@ pub fn hex_number(input: &[u8]) -> IResult<&[u8], u8> {
     )(input)
 }
 
+pub fn percent(input: &[u8]) -> IResult<&[u8], char> {
+    nom::character::complete::char('%')(input)
+}
+
 pub fn escaped(input: &[u8]) -> IResult<&[u8], u8> {
-    let (s, _) = tag("%")(input)?;
+    let (s, _) = percent(input)?;
     hex_number(s)
 }
 
-pub fn is_horizontal_tab(i: u8) -> bool {
-    i == 0x09
-}
+const WHITESPACE_CHARS: &'static [u8] = b" \t";
 
-pub fn is_space(i: u8) -> bool {
-    i == 0x20
-}
-
-pub fn is_whitespace(i: u8) -> bool {
-    is_space(i) || is_horizontal_tab(i)
-}
-
-pub fn whitespace(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    take_while(is_whitespace)(input)
-}
-
-pub fn whitespace1(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    take_while1(is_whitespace)(input)
+pub fn whitespace(input: &[u8]) -> IResult<&[u8], char> {
+    one_of(WHITESPACE_CHARS)(input)
 }
 
 pub fn newline(input: &[u8]) -> IResult<&[u8], &[u8]> {
@@ -114,8 +130,8 @@ pub fn newline(input: &[u8]) -> IResult<&[u8], &[u8]> {
 pub fn linear_whitespace(input: &[u8]) -> IResult<&[u8], &[u8]> {
     recognize(
         pair(
-            opt(pair(whitespace, newline)),
-            whitespace1
+            opt(pair(many0(whitespace), newline)),
+            whitespace
         )
     )(input)
 }
@@ -127,7 +143,7 @@ pub fn separator_whitespace(input: &[u8]) -> IResult<&[u8], &[u8]> {
 pub fn header_colon(input: &[u8]) -> IResult<&[u8], &[u8]> {
     recognize(
         tuple((
-                whitespace,
+                many0(whitespace),
                 nom::character::complete::char(':'),
                 separator_whitespace
         ))
@@ -390,7 +406,7 @@ pub fn utf8_trim(input: &[u8]) -> IResult<&[u8], &[u8]> {
     recognize(
         pair(
             utf8_char1,
-            many0(pair(opt(linear_whitespace), utf8_char1))
+            many0(pair(many0(linear_whitespace), utf8_char1))
         )
     )(input)
 }
@@ -401,12 +417,12 @@ pub fn is_comment_char(i: u8) -> bool {
         (i >= 0x5d && i <= 0x7e)
 }
 
-pub fn comment_char1(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    take_while1(is_comment_char)(input)
+pub fn comment_char(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    take_while_m_n(1, 1, is_comment_char)(input)
 }
 
 pub fn comment_text(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    alt((comment_char1, utf8_nonascii1))(input)
+    alt((comment_char, utf8_nonascii1, linear_whitespace))(input)
 }
 
 pub fn comment(input: &[u8]) -> IResult<&[u8], &[u8]> {
@@ -414,13 +430,13 @@ pub fn comment(input: &[u8]) -> IResult<&[u8], &[u8]> {
         tuple((
                 left_parenthesis,
                 many0(alt((comment_text, comment, quoted_pair))),
+                right_parenthesis,
         ))
     )(input)
 }
 
 pub fn is_quotable_character(i: u8) -> bool {
-    i <= 0x09 ||
-        (i >= 0x0b && i <= 0x0c) ||
+    i <= 0x09 || i == 0x0b || i == 0x0c ||
         (i >= 0x0e && i <= 0x7f)
 }
 
@@ -471,43 +487,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn is_reserved_identifies_reserved_chars() {
-        b";/?:@&=+$,".iter().for_each(|i| assert_eq!(is_reserved(*i), true));
-        assert_eq!(is_reserved(b'z'), false);
-    }
-
-    #[test]
-    fn reserved_consumes_reserved_chars() {
-        assert!(reserved(b";/?:@&=+$,") == Ok((b"", b";/?:@&=+$,")));
-        assert!(reserved(b";/?:xx@&=+$,") == Ok((b"xx@&=+$,", b";/?:")));
-        assert!(reserved(b";;;;;;foo") == Ok((b"foo", b";;;;;;")))
-    }
-
-    #[test]
-    fn is_mark_identifies_mark_chars() {
-        b"-_.!~*'()".iter().for_each(|i| assert_eq!(is_mark(*i), true));
-        assert_eq!(is_mark(b'z'), false);
+    fn reserved_consumes_reserved_char() {
+        assert!(reserved(b";/?:@&=+$,") == Ok((b"/?:@&=+$,", ';')));
+        assert!(reserved(b"?;;;;;;foo") == Ok((b";;;;;;foo", '?')));
+        assert_eq!(reserved(b"xx@&=+$,").is_err(), true);
     }
 
     #[test]
     fn mark_consumes_mark_chars() {
-        assert!(mark(b"-_.!~*'()") == Ok((b"", b"-_.!~*'()")));
-        assert!(mark(b"-_.!xx~*'()") == Ok((b"xx~*'()", b"-_.!")));
-        assert!(mark(b"______foo") == Ok((b"foo", b"______")));
-    }
-
-    #[test]
-    fn is_unreserved_identifies_unreserved_chars() {
-        b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.!~*'()"
-            .iter().for_each(|i| assert_eq!(is_unreserved(*i), true));
-        assert_eq!(is_unreserved(b';'), false);
+        assert!(mark(b"-_.!~*'()") == Ok((b"_.!~*'()", '-')));
+        assert!(mark(b"______foo") == Ok((b"_____foo", '_')));
+        assert_eq!(mark(b"xx!-_.xx~*'()").is_err(), true);
     }
 
     #[test]
     fn unreserved_consumes_unreserved_chars() {
-        let chars = b"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.!~*'()";
-        assert!(unreserved(chars) == Ok((b"", chars)));
-        assert!(unreserved(b"aaaaa$") == Ok((b"$", b"aaaaa")));
+        assert!(unreserved(b"abc") == Ok((b"bc", 'a')));
+        assert!(unreserved(b"Zf") == Ok((b"f", 'Z')));
+        assert_eq!(unreserved(b"$f").is_err(), true);
     }
 
     #[test]
@@ -517,16 +514,6 @@ mod tests {
             .for_each(|i| assert_eq!(is_hex_digit(*i), true));
 
         assert_eq!(is_hex_digit(b';'), false);
-    }
-
-    #[test]
-    fn is_lowercase_hex_digit_identifies_only_lower() {
-        b"1234567890abcdef"
-            .iter()
-            .for_each(|i| assert_eq!(is_lowercase_hex_digit(*i), true));
-
-        assert_eq!(is_lowercase_hex_digit(b','), false);
-        assert_eq!(is_lowercase_hex_digit(b'F'), false);
     }
 
     #[test]
@@ -553,28 +540,9 @@ mod tests {
     }
 
     #[test]
-    fn is_space_validates_spaces() {
-        assert_eq!(is_space(b' '), true);
-        assert_eq!(is_space(b'0'), false);
-    }
-
-    #[test]
-    fn is_whitespace_validates_whitespaces() {
-        assert_eq!(is_whitespace(b' '), true);
-        assert_eq!(is_whitespace(b'\t'), true);
-        assert_eq!(is_whitespace(b'0'), false);
-    }
-
-    #[test]
-    fn whitespace_consumes_0_or_more_whitespaces() {
-        assert!(whitespace(b"  \t  foo") == Ok((b"foo", b"  \t  ")));
-        assert!(whitespace(b"foo") == Ok((b"foo", b"")));
-    }
-
-    #[test]
-    fn whitespace1_consumes_1_or_more_whitespaces() {
-        assert!(whitespace1(b"  \t  foo") == Ok((b"foo", b"  \t  ")));
-        assert_eq!(whitespace1(b"foo").is_err(), true);
+    fn whitespace_consumes_1_whitespaces() {
+        assert!(whitespace(b"  \t  foo") == Ok((b" \t  foo", ' ')));
+        assert_eq!(whitespace(b"foo").is_err(), true);
     }
 
     #[test]
@@ -585,18 +553,18 @@ mod tests {
 
     #[test]
     fn linear_whitespace_requires_at_least_whitespace() {
-        assert!(linear_whitespace(b"  f") == Ok((b"f", b"  ")));
+        assert!(linear_whitespace(b"  f") == Ok((b" f", b" ")));
         assert_eq!(linear_whitespace(b"x").is_err(), true);
     }
 
     #[test]
     fn linear_whitespace_eats_preceding_crlf() {
-        assert!(linear_whitespace(b"\r\n  f") == Ok((b"f", b"\r\n  ")));
+        assert!(linear_whitespace(b"\r\n f") == Ok((b"f", b"\r\n ")));
     }
 
     #[test]
     fn linear_whitespace_eats_preceding_ws_and_crlf() {
-        assert!(linear_whitespace(b"\t\t  \t \t \t\r\n  \tf") == Ok((b"f", b"\t\t  \t \t \t\r\n  \t")));
+        assert!(linear_whitespace(b"\t\t  \t \t \t\r\n\tf") == Ok((b"f", b"\t\t  \t \t \t\r\n\t")));
     }
 
     #[test]
