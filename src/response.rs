34c05047cb666f71bd9d5788363dcd1fa4edc426
Sebastian Lauwers <sl@unticks.com>
2020-03-01 20:39:19 +0100

    Add documentation for Response

--- src/response.rs
+++ src/response.rs
@@ -1,12 +1,19 @@
 use crate::parser::rfc3261;
 use crate::parser::{ Error, ErrorKind, };
 
+/// Representation of a SIP Response
+///
+/// **Note**: Responses are currently not well-supported. Patches welcome!
 #[derive(PartialEq, Debug, Copy, Clone)]
 pub struct Response<'a> {
+    /// unparsed content of the Response
     pub content: &'a str,
 }
 
 impl<'a> Response<'a> {
+    /// Attempts to parse a byte-slice representation of a SIP response
+    ///
+    /// **Note**: Responses are currently not parsed in detail.
     pub fn parse(input: &'a [u8]) -> Result<Self, Error<'a, &[u8]>> {
         match rfc3261::response(input) {
             Ok((_, req)) => Ok(req),
